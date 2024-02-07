40b15a976fb310d5ecf3de18fb0c0ea55545abd4
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-07 19:56:40 +0100

    assets

---
+++ assets/svg/ImageIcon.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const ImageIcon = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="mx-auto"
+    >
+      <g clip-path="url(#clip0_148_428)">
+        <path
+          d="M20 10.6667H20.0133"
+          stroke="#25324B"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M22.6663 5.33331H9.33301C7.12387 5.33331 5.33301 7.12417 5.33301 9.33331V22.6666C5.33301 24.8758 7.12387 26.6666 9.33301 26.6666H22.6663C24.8755 26.6666 26.6663 24.8758 26.6663 22.6666V9.33331C26.6663 7.12417 24.8755 5.33331 22.6663 5.33331Z"
+          stroke="#25324B"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M5.33301 20L10.6663 14.6667C11.2744 14.0815 11.9642 13.7735 12.6663 13.7735C13.3685 13.7735 14.0583 14.0815 14.6663 14.6667L21.333 21.3333"
+          stroke="#25324B"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M18.667 18.6667L20.0003 17.3334C20.6084 16.7482 21.2982 16.4402 22.0003 16.4402C22.7025 16.4402 23.3922 16.7482 24.0003 17.3334L26.667 20"
+          stroke="#25324B"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_148_428">
+          <rect width="32" height="32" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default ImageIcon;
