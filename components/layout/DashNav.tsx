c718811c93fde4585b1b45db6d5e4722fdd6ef8b
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-08 11:40:42 +0100

    chore: profile icon dropdown

--- components/layout/DashNav.tsx
+++ components/layout/DashNav.tsx
@@ -1,8 +1,35 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { CustomSelect as Select } from "@/lib/AntdComponents";
+import type { MenuProps } from "antd";
+import { Dropdown, Space } from "antd";
+
 import { CgMenuRightAlt } from "react-icons/cg";
 import { CiSearch } from "react-icons/ci";
-import logo from "@/assets/png/People@2x.png";
-import Image from "next/image";
+import avatar from "@/assets/png/Avatar.png";
+import NotificationIcon from "@/assets/svg/NotificationIcon";
+
+const items: MenuProps["items"] = [
+  {
+    label: <Link href="#">Profile</Link>,
+    key: "0",
+  },
+  {
+    label: <Link href="#">Settings</Link>,
+    key: "1",
+  },
+  {
+    type: "divider",
+  },
+  {
+    label: <Link href="#">Logout</Link>,
+    key: "3",
+  },
+];
+
 const DashNav = () => {
   return (
     <>
@@ -21,16 +48,39 @@ const DashNav = () => {
           <input
             type="text"
             placeholder="Search"
-            className="w-full md:w-[320px] rounded-md border-gray-200 border py-2 pe-0  sm:text-sm pl-8 ring-0   bg-inherit"
+            className="w-full md:w-[520px] rounded-md border-gray-200 border py-2 pe-0  sm:text-sm pl-8 ring-0   bg-inherit"
           />
         </div>
         <div className="space-x-8 items-center flex">
-          <div className="hidden lg:flex space-x-3 py-2  px-4 items-center border border-gray-700 rounded-full">
-            <span className="w-2 h-2 rounded-full bg-green-700"></span>
-            <span>Notifications</span>
+          <div className="hidden lg:block">
+            <Select
+              id="create"
+              defaultValue="create new"
+              options={[
+                { value: "create new", label: "Create new business" },
+                { value: "existing", label: "Existing business" },
+              ]}
+              className="p-2 border w-full rounded-md border-none  bg-white text-sm text-[#A7A9AD] shadow-none"
+            />
           </div>
-          <div className="avatar">
-            <Image alt="" src={logo} />
+          <button
+            type="button"
+            className="hidden lg:flex space-x-3 p-2 items-center border border-[#EEF1F6] bg-white rounded-full"
+          >
+            {/* <span className="w-2 h-2 rounded-full bg-green-700"></span> */}
+            <NotificationIcon />
+          </button>
+          <div className="">
+            <Dropdown
+              menu={{ items }}
+              trigger={["click"]}
+              className="hover:cursor-pointer"
+            >
+              <a onClick={(e) => e.preventDefault()}>
+                <Image alt="Avatar" src={avatar} className="w-12 h-12" />
+              </a>
+            </Dropdown>
+            {/* <Image alt="Avatar" src={avatar} className="w-12 h-12" /> */}
           </div>
         </div>
       </nav>
