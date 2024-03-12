25b310c87ddca27213c3d828c8036ada98c81c8c
oluwabukola2610 <roheemohmudashir@gmail.com>
2024-03-12 09:25:37 +0100

    new changes

---
+++ services/users/index.service.ts
@@ -0,0 +1,19 @@
+import { baseQueryWithReauth } from "@/constant";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+export const userApi = createApi({
+  reducerPath: "userApi",
+  refetchOnReconnect: true,
+  refetchOnMountOrArgChange: true,
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({
+    profile: builder.query({
+      query: () => ({
+        url: "api/v1/users/user/1",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useProfileQuery, useLazyProfileQuery } = userApi;
