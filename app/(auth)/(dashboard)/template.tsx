3a30ee34c5dc4c39b61855d506a8acf0436cd82c
sikiru razak <sikiru@nubytes.us>
2024-03-15 12:55:39 +0100

    update get user endpoint

--- app/(auth)/(dashboard)/template.tsx
+++ app/(auth)/(dashboard)/template.tsx
@@ -1,7 +1,9 @@
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import React from "react";
+import { useProfileQuery } from "@/services/users/index.service";
 
 const template = ({ children }: { children: React.ReactNode }) => {
+  useProfileQuery({});
   return <DashboardLayout>{children}</DashboardLayout>;
 };
 
