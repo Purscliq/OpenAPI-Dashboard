8426872ac17c2065b94cba5e190e6f1530b15b63
sikiru razak <sikiru@nubytes.us>
2024-03-15 13:21:58 +0100

    update user profile

--- app/(auth)/(dashboard)/template.tsx
+++ app/(auth)/(dashboard)/template.tsx
@@ -1,10 +1,27 @@
+"use client";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import React from "react";
 import { useProfileQuery } from "@/services/users/index.service";
+import Image from "next/image";
+import logo from "@/assets/svg/logo.svg";
 
 const template = ({ children }: { children: React.ReactNode }) => {
-  useProfileQuery({});
-  return <DashboardLayout>{children}</DashboardLayout>;
+  const { isLoading, isUninitialized, isSuccess } = useProfileQuery({});
+  return (
+    <>
+      {(isLoading || isUninitialized) && isSuccess ? (
+        <div className="relative h-screen flex items-center justify-center bg-[#FAFAFA]">
+          <div className="fixed top-0 left-0 px-6 py-4">
+            <Image src={logo} alt="logo" className="w-28 h-28" />
+          </div>
+          <div className="fixed inset-0 bg-black opacity-50 z-50" />
+          <div className="w-16 h-16 border-t-4 border-black border-solid rounded-full animate-spin z-50" />
+        </div>
+      ) : (
+        <DashboardLayout>{children}</DashboardLayout>
+      )}
+    </>
+  );
 };
 
 export default template;
--- services/users/index.service.ts
+++ services/users/index.service.ts
@@ -1,5 +1,6 @@
 import { baseQueryWithReauth } from "@/constant";
 import { createApi } from "@reduxjs/toolkit/query/react";
+import { updateUser } from "@/slice/userSlice";
 
 export const userApi = createApi({
   reducerPath: "userApi",
@@ -12,6 +13,16 @@ export const userApi = createApi({
         url: "api/v1/users/user",
         method: "GET",
       }),
+      onQueryStarted(id, { dispatch, queryFulfilled }) {
+        queryFulfilled
+          .then((apiResponse) => {
+            console.log(apiResponse);
+            dispatch(updateUser(apiResponse?.data?.data));
+          })
+          .catch(() => {
+            location.pathname = "/";
+          });
+      },
     }),
   }),
 });
