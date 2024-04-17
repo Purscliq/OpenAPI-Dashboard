8173e473c8ac4a864528601368064ed07dc6ff26
oluwabukola2610 <roheemohmudashir@gmail.com>
2024-04-17 09:10:56 +0100

    fix dashboard issue

--- components/dashboard-component/developers/Tabs/APIKeysTab.tsx
+++ components/dashboard-component/developers/Tabs/APIKeysTab.tsx
@@ -12,7 +12,7 @@ const APIKeysTab = () => {
   };
 
   return (
-    <section className="bg-white py-4 px-4 space-y-4">
+    <section className="bg-white px-4 space-y-4">
       <CreateAPIKeyModal onApiKeyCreated={refreshApiKeys} />
       <APIKeyTable shouldRefresh={shouldRefresh} />
     </section>
