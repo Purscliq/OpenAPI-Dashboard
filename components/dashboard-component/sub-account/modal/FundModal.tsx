e8319a44a40d71ea1296b0e8184a4c18432dbcb7
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-04-23 18:24:07 +0100

    fund and withdraw modals

--- components/dashboard-component/sub-account/modal/FundModal.tsx
+++ components/dashboard-component/sub-account/modal/FundModal.tsx
@@ -1,26 +1,97 @@
-import { Modal } from 'antd'
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
+import { CustomTooltip as Tooltip } from "@/lib/AntdComponents";
+
+import { useGetSubaccountQuery } from "@/services/business/index.service";
+
 interface FundModalProps {
   openFundsModal: boolean;
   close: () => void;
   handleOpenWithdrawalModal: () => void;
 }
 
-const FundModal: React.FC<FundModalProps> = ({ openFundsModal, close, handleOpenWithdrawalModal }) => {
-  return (
-       <Modal
-        title="Details"
-        open={openFundsModal}
-        onCancel={close}
-        footer={[
-          <button key="withdraw" onClick={handleOpenWithdrawalModal}>
-            Withdraw
-          </button>,
-        ]}
-      >
-        <p>Modal content</p>
-      </Modal>
-  )
-}
+const FundModal: React.FC<FundModalProps> = ({
+  openFundsModal,
+  close,
+  handleOpenWithdrawalModal,
+}) => {
+  const [toogleTooltip, setToogleTooltip] = useState(false);
+  const [isFundModalOpen, setIsFundModalOpen] = useState(false);
+  const { data: subAccountData } = useGetSubaccountQuery({});
 
-export default FundModal
+  return (
+    <Modal
+      // title="Details"
+      open={openFundsModal}
+      onCancel={close}
+      footer={[
+        <button
+          key="withdraw"
+          onClick={handleOpenWithdrawalModal}
+          className="bg-[#010101] text-white px-[24px] py-[12px] rounded-[5px]"
+        >
+          Withdraw
+        </button>,
+      ]}
+    >
+      <div className="flex flex-col gap-4">
+        <article className="flex flex-col space-y-4 bg-white p-[2%]">
+          <h2 className="text-[32px] text-black font-bold text-center pt-5">
+            Fund Sub-Account
+          </h2>
+          <div className="flex justify-end items-end">
+            <Tooltip title="copied!" trigger={"click"} open={toogleTooltip}>
+              <Button
+                onClick={() => {
+                  setToogleTooltip(true);
+                  navigator.clipboard
+                    .writeText(
+                      `Bank Name: ${subAccountData?.data?.bank_name} \n Account Name: ${subAccountData?.data?.account_name} \n Account Number: ${subAccountData?.data?.account_number}`
+                    )
+                    .finally(() => {
+                      setTimeout(() => {
+                        setToogleTooltip(false);
+                      }, 2000);
+                    });
+                }}
+                className="text-lg !font-semibold !border-none"
+              >
+                + Copy
+              </Button>
+            </Tooltip>
+          </div>
+          <div className="bg-[#FAFAFA] p-3 flex flex-col gap-4">
+            <span className="flex justify-between items-center">
+              <p className="text-[#515B6F]">Bank Name</p>
+              <p className="text-[#181336] font-semibold">
+                {subAccountData?.data?.bank_name || "First Bank"}
+              </p>
+            </span>
+            <span className="flex gap-[0.2rem] justify-between items-center">
+              <p className="text-[#515B6F]">Account Name</p>
+              <p className="text-[#181336] font-semibold">
+                {subAccountData?.data?.account_name || "John David Doe"}
+              </p>
+            </span>
+            <span className="flex justify-between items-center">
+              <p className="text-[#515B6F]">Account Number</p>
+              <p className="text-[#181336] font-semibold">
+                {subAccountData?.data?.account_number || "9955667794"}
+              </p>
+            </span>
+            <span className="flex justify-between items-center">
+              <p className="text-[#515B6F]">Account Alias</p>
+              <p className="text-[#181336] font-semibold">
+                {subAccountData?.data?.account_type || "Purs main current account"}
+              </p>
+            </span>
+          </div>
+        </article>
+      </div>
+    </Modal>
+  );
+};
+
+export default FundModal;
--- components/dashboard-component/sub-account/modal/WithdrawalModal.tsx
+++ components/dashboard-component/sub-account/modal/WithdrawalModal.tsx
@@ -1,5 +1,6 @@
-import { Modal } from "antd";
 import React from "react";
+import { Modal } from "antd";
+import { CustomSelect as Select } from "@/lib/AntdComponents";
 interface ModalProps {
   openWithdrawalModal: boolean;
   close: () => void;
@@ -11,12 +12,111 @@ const WithdrawalModal: React.FC<ModalProps> = ({
 }) => {
   return (
     <Modal
-      title="Withdrawal"
+      // title="Withdrawal"
       open={openWithdrawalModal}
       onCancel={close}
       footer={null}
     >
-      <p>Withdrawal modal content goes here...</p>
+      <div className="pt-5 space-y-6">
+        <span className="flex flex-col justify-center">
+          <h2 className="md:text-[32px] text-[18px] text-black font-bold text-center">
+            Withdraw Account
+          </h2>
+          <p className="text-center text-[14px] md:text-[18px] text-[#515B6F]">
+            Enter the details to withdrawal from account
+          </p>
+        </span>
+
+        <form className="space-y-4">
+          <span className="flex flex-col gap-1">
+            <label
+              htmlFor="account"
+              className="text-[#24272C] text-base font-bold"
+            >
+              Select Account
+            </label>
+            <Select
+              placeholder=""
+              className="!w-full !h-[40px]"
+              id="account"
+              options={[
+                { value: "account1", label: "Account1" },
+                { value: "account2", label: "Account2" },
+                { value: "account3", label: "Account3" },
+              ]}
+            />
+          </span>
+          <span className="flex flex-col gap-1">
+            <label
+              htmlFor="bank"
+              className="text-[#24272C] text-base font-bold"
+            >
+              Select Bank
+            </label>
+            <Select
+              placeholder=""
+              className="!w-full !h-[40px]"
+              id="bank"
+              options={[
+                { value: "bank1", label: "Bank1" },
+                { value: "bank2", label: "Bank2" },
+                { value: "bank3", label: "Bank3" },
+              ]}
+            />
+          </span>
+          <span className="flex flex-col gap-1">
+            <label
+              htmlFor="accountNo"
+              className="text-[#24272C] text-base font-bold"
+            >
+              Account Number
+            </label>
+            <input
+              type="number"
+              id="accountNo"
+              className="w-full rounded-[5px] px-[8px] pr-[16px] h-[50px] text-[14px] border border-[#E9EBEB]"
+              required
+            />
+            <p className="text-[#515B6F] text-[14px]">Temitope williams</p>
+          </span>
+          <span className="flex flex-col gap-1">
+            <label
+              htmlFor="amount"
+              className="text-[#24272C] text-base font-bold"
+            >
+              Amount
+            </label>
+            <input
+              type="number"
+              id="amount"
+              className="w-full rounded-[5px] px-[8px] pr-[16px] h-[50px] text-[14px] border border-[#E9EBEB]"
+              required
+            />
+          </span>
+          <span className="flex flex-col gap-1">
+            <label
+              htmlFor="narration"
+              className="text-[#24272C] text-base font-bold"
+            >
+              Narration
+            </label>
+            <input
+              type="text"
+              id="narration"
+              className="w-full rounded-[5px] px-[8px] pr-[16px] h-[50px] text-[14px] border border-[#E9EBEB]"
+              required
+            />
+          </span>
+          <span className="flex md:justify-end pt-4">
+            <button
+              type="submit"
+              className="bg-[#010101] text-white px-[24px] py-[12px] rounded-[5px]"
+            >
+              Withdraw
+            </button>
+          </span>
+        </form>
+      </div>
     </Modal>
   );
 };
